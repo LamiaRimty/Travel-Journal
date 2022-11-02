a4132e0f7200d68318d61e54526752864c12425c
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2022-11-02 18:52:15 +0100

    render home.ejs

--- app.js
+++ app.js
@@ -11,9 +11,27 @@ app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const homeStartingContent="I'm a Web Developer.I ❤️ painting and travelling";
+
+const aboutContent="I'm from Bangladesh and completed gradutation from Computer Science & Engineering of The University of Rajshahi.Over the time,I have gained a growing of experience designing and developing web applications."
+
+const contactContent="Get In Touch.If you love reading as much I do.If you love reading as much I do? Let's talk about coding & some books!.My Email: rimtycse@email.com"
+
+
 app.get("/",function(req,res){
-    res.send("server is up & running");
+  res.render("home",{ homeContent: homeStartingContent });
 });
+
+app.get("/about",function(req,res){
+    console.log("1");
+    res.render("header",{ aboutStaringContent : aboutContent});
+});
+
+app.post("/about",function(req,res){
+    console.log("2");
+    res.redirect("/about");
+});
+
 
 app.listen(3000,function(){
     console.log("Server is running on port 3000");
